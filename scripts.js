7f155ab6f01c6f3fdb300d32c1f28f868ab489bb
Theo Roberts <theo10roberts@gmail.com>
2023-08-09 11:15:14 +0100

    changed job descriptions

--- scripts.js
+++ scripts.js
@@ -9,19 +9,19 @@ function neptikJobDescriptionOn(){
     const neptik = document.querySelector('#jobDescription');
     const neptikJobDescription1 = document.createElement('li');
     neptikJobDescription1.classList.add('neptikJobDescription');
-    neptikJobDescription1.textContent = 'Day to day, my role included gathering and cleansing large amounts of data, managing multiple inboxes, building daily email campaigns and monitoring their performance. This required me to be organised and independent in managing my workload.';
+    neptikJobDescription1.textContent = 'Gathered and cleansed large amounts of data, managed multiple inboxes, built daily email campaigns and monitored their performance. This required me to be organised and independent in managing my workload. Tasks were time-sensitive, developing my ability to work under pressure whilst maintaining a high level of quality.';
     neptik.appendChild(neptikJobDescription1);
     const neptikJobDescription2 = document.createElement('li');
     neptikJobDescription2.classList.add('neptikJobDescription');
-    neptikJobDescription2.textContent = 'I independently created a series of Macros to help increase efficiency. They hugely reduced processing times across the whole team, and was praised by management.';
+    neptikJobDescription2.textContent = 'Cut processing times across the whole team by independently developing a series of Macros. Implemented training for all team members and created workflows and guides to accompany them.';
     neptik.appendChild(neptikJobDescription2);
     const neptikJobDescription3 = document.createElement('li');
     neptikJobDescription3.classList.add('neptikJobDescription');
-    neptikJobDescription3.textContent = 'Created monthly performance reports for clients before attending video calls. Gained confidence in communicating the relevant data and information to the client.';
+    neptikJobDescription3.textContent = 'Often called upon to help out with tasks outside of my usual responsibility. This included training new members of the team and creating an analysis of subject lines used in marketing emails.';
     neptik.appendChild(neptikJobDescription3);
     const neptikJobDescription4 = document.createElement('li');
     neptikJobDescription4.classList.add('neptikJobDescription');
-    neptikJobDescription4.textContent = 'Tasks were time-sensitive, developing my ability to work under pressure whilst maintaining a high level of quality. When colleagues needed help we shared the workload, so I am used to picking up extra tasks when required. ';
+    neptikJobDescription4.textContent = 'Created monthly performance reports for clients before attending video calls. Gained confidence in communicating the relevant data and information to the client.';
     neptik.appendChild(neptikJobDescription4);
 }
 
@@ -40,17 +40,22 @@ function neptikJobDescriptionOff(){
 
 function pizzaExpressJobDescriptionOn(){
     const pizzaExpress = document.querySelector('#jobDescription');
-    const pizzaExpressJobDescription = document.createElement('li');
-    pizzaExpressJobDescription.classList.add('pizzaExpressJobDescription');
-    pizzaExpressJobDescription.textContent = 'For just over a year I worked as a waiter in a busy central Brighton restaurant. It was full each weekend and during school holidays, so I got used to fast-paced work environments. This meant I became very good at multitasking and looking after a high number of tables at once.'
-    pizzaExpressJobDescription.style.cssText = ('transition: 1s');
-    pizzaExpress.appendChild(pizzaExpressJobDescription);
+    const pizzaExpressJobDescription1 = document.createElement('li');
+    pizzaExpressJobDescription1.classList.add('pizzaExpressJobDescription');
+    pizzaExpressJobDescription1.textContent = 'Took orders, ran food, made coffees and prepared desserts. It was full each weekend and during school holidays, so I got used to fast-paced work environments. This meant I became very good at multitasking and looking after a high number of tables at once.'
+    pizzaExpress.appendChild(pizzaExpressJobDescription1);
+    const pizzaExpressJobDescription2 = document.createElement('li');
+    pizzaExpressJobDescription2.classList.add('pizzaExpressJobDescription');
+    pizzaExpressJobDescription2.textContent = 'Often trained new members of the team, and left to run the restaurant floor.'
+    pizzaExpress.appendChild(pizzaExpressJobDescription2);
 }
 
 function pizzaExpressJobDescriptionOff(){
     const pizzaExpress = document.querySelector('#jobDescription');
-    const pizzaExpressJobDescription = document.querySelector('li')
-    pizzaExpress.removeChild(pizzaExpressJobDescription);
+    const pizzaExpressJobDescription1 = document.querySelector('li')
+    pizzaExpress.removeChild(pizzaExpressJobDescription1);
+    const pizzaExpressJobDescription2 = document.querySelector('li')
+    pizzaExpress.removeChild(pizzaExpressJobDescription2);
 }
 
 
@@ -58,11 +63,11 @@ function totalJobDescriptionOn(){
     const total = document.querySelector('#jobDescription');
     const totalJobDescription1 = document.createElement('li');
     totalJobDescription1.classList.add('totalJobDescription');
-    totalJobDescription1.textContent = 'This role involved considerable attention to detail, daily tasks involved data entry, checking high volumes of information and handling queries.'
+    totalJobDescription1.textContent = 'Daily tasks involved data entry, checking high volumes of information and handling queries.'
     total.appendChild(totalJobDescription1);
     const totalJobDescription2 = document.createElement('li');
     totalJobDescription2.classList.add('totalJobDescription');
-    totalJobDescription2.textContent = 'I communicated via email and phone call with key stakeholders daily, helping me to develop interpersonal and communication skills. I also worked from home for a large part of my time at Total and adapted to using Microsoft Teams to keep in touch with my colleagues.'
+    totalJobDescription2.textContent = 'Communicated via email and phone call with key stakeholders daily, helping me to develop interpersonal and communication skills. I also worked from home for a large part of my time at Total and adapted to using Microsoft Teams to keep in touch with my colleagues.'
     total.appendChild(totalJobDescription2);
 }
 
